ef33e8704466b6bcd58657c8cb11bc397941e284
Brian Silvey <blujay270@gmail.com>
2015-11-13 14:30:11 -0500

    added dynamic changing spaces

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 SC.initialize({
-    client_id: 'YOUR_CLIENT_ID'
+    client_id: '75f4709735ae050afcaea64eecd3af51'
 });
 
-var track_url = 'https://soundcloud.com/niagaramoon/solo-for-two';
-SC.oEmbed(track_url, {
-    element: document.getElementById('firstTrack')
-});
+function embed() {
+    // find all sounds of buskers licensed under 'creative commons share alike'
+    SC.get('/users/niagaramoon/tracks', {}).then(function(tracks) {
+        console.log(tracks);
+        var track_url = tracks[0].permalink_url;
+        console.log(track_url);
+        SC.oEmbed(track_url, { 
+            auto_play: true,
+            element: document.getElementById('firstTrack') 
+        });
+        track_url = tracks[1].permalink_url;
+        console.log(track_url);
+        SC.oEmbed(track_url, { 
+            auto_play: true,
+            element: document.getElementById('secondTrack') 
+        });
+        var track_url = tracks[2].permalink_url;
+        console.log(track_url);
+        SC.oEmbed(track_url, { 
+            auto_play: true,
+            element: document.getElementById('thirdTrack') 
+        });
+    });
+}
+
+$(document).ready(
+    function() {
+        embed();
+    }
+)
