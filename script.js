ba9d9482f79564af06f25fa7037dbb2492b50a7b
Brian Silvey <blujay270@gmail.com>
2015-11-13 21:55:38 -0500

    working on background image

--- script.js
+++ script.js
@@ -5,23 +5,19 @@ SC.initialize({
 function embed() {
     // find all sounds of buskers licensed under 'creative commons share alike'
     SC.get('/users/niagaramoon/tracks', {}).then(function(tracks) {
-        console.log(tracks);
         var track_url = tracks[0].permalink_url;
-        console.log(track_url);
         SC.oEmbed(track_url, { 
-            auto_play: true,
+            auto_play: false,
             element: document.getElementById('firstTrack') 
         });
         track_url = tracks[1].permalink_url;
-        console.log(track_url);
         SC.oEmbed(track_url, { 
-            auto_play: true,
+            auto_play: false,
             element: document.getElementById('secondTrack') 
         });
         var track_url = tracks[2].permalink_url;
-        console.log(track_url);
         SC.oEmbed(track_url, { 
-            auto_play: true,
+            auto_play: false,
             element: document.getElementById('thirdTrack') 
         });
     });
